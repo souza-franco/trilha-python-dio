3a99c4ef0be3233d2405737f9709e7c218a233d4
Michael Henrique Souza Franco <122502158+souza-franco@users.noreply.github.com>
2023-09-17 15:58:35 -0400

    a

--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,10 @@
-lista = [1, "Python", [40, 30, 20]]
+lista = [10, "Python", [40, 30, 20]]
 
-lista.copy()
+lista2 = lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista2[0] = 2
+
+print(lista2)
+print(lista)
