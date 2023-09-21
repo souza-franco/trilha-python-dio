c1b9512cbbbdd13bd95c3a8958c763297fee7883
Michael Henrique Souza Franco <122502158+souza-franco@users.noreply.github.com>
2023-09-20 22:17:25 -0400

    a

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -1,10 +1,13 @@
 salario = 2000
 
 
-def salario_bonus(bonus):
+def salario_bonus(bonus, lista):
     global salario
+    lista.copy(2)
     salario += bonus
     return salario
 
-
-salario_bonus(500)  # 2500
+lista = [1]
+salario_com_bonus = salario_bonus(500)  # 2500
+print(salario_com_bonus)
+print(lista)
